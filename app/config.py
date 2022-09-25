accd92d6581901cad328b74dbc409ede6174945f
JeanExtreme002 <jeangamerextreme@gmail.com>
2022-09-25 19:24:30 -0300

    added sounds

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+sound_path = "sounds"
+sound_effects_path = os.path.join(sound_path, "effects")
+music_path = os.path.join(sound_path, "music")
+
+image_path = "images"
+
+database_path = "database"
