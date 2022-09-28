d59dbc63fee385d89d8360c297ff5961f022f78f
JeanExtreme002 <jeangamerextreme@gmail.com>
2022-09-28 02:37:00 -0300

    added image to sidebar

--- app/screens/home.py
+++ app/screens/home.py
@@ -16,26 +16,28 @@ class HomeScreen(Screen):
         background_height = self.height
 
         # Obtém tamanho da logo.
-        logo_width = self.get_pixels_by_percent(10, 0)[0]
-        logo_height = logo_width
+        logo_width = self.get_pixels_by_percent(20, 0)[0]
+        logo_height = logo_width // 1.5
 
-        # Desenha background da barra lateral.
-        shape = self.create_rectangle(0, 0, background_x - 2, self.height, color = (30, 30, 80), batch = batch)
+        # Carrega a imagem da barra lateral.
+        sidebar_filename = application.paths.get_image("home", "sidebar.png")
+        sidebar_image = self.load_image(sidebar_filename, (background_x - 2, self.height))
     
-        # Carrega a imagem de logo e desenha.
-        logo_filename = application.paths.get_image("home_logo.png")
+        # Carrega a imagem de logo.
+        logo_filename = application.paths.get_image("home", "logo.png")
         logo_image = self.load_image(logo_filename, (logo_width, logo_height))
         
         logo_sprite = self.create_sprite(
             logo_image, batch = batch,
             x = int(background_x / 2 - logo_width / 2),
-            y = self.get_pixels_by_percent(0, 10)[1] + logo_width / 2
+            y = self.get_pixels_by_percent(0, 7)[1] + logo_width // 2
         )
 
         # Carrega a imagem de background.
-        background_filename = application.paths.get_random_image("background")
+        background_filename = application.paths.get_random_image("home", "background")
         background_image = self.load_image(background_filename, (background_width, background_height))
 
         # Aplica todos os desenhos na tela.
+        sidebar_image.blit(0, 0)
         background_image.blit(background_x, background_y)
         batch.draw()
