bb0ed1e789111b29539526abb4d7da9f6af34699
JeanExtreme002 <jeangamerextreme@gmail.com>
2022-10-10 03:55:00 -0300

    added history and achivements buttons

--- app/screens/home.py
+++ app/screens/home.py
@@ -34,20 +34,21 @@ class HomeScreen(Screen):
         logo_x = int(sidebar_width * 0.5 - logo_width * 0.5)
         logo_y = int(self.height * 0.02)
 
-        # Obtém o tamanho e posição dos botões de jogar.
-        play_button_width = int(sidebar_width * 0.60)
-        play_button_height = int(play_button_width * 0.39)
+        # Obtém o tamanho e posição dos botões maiores, que serão dispostos verticalmente.
+        large_button_width = int(sidebar_width * 0.60)
+        large_button_height = int(large_button_width * 0.39)
+        large_button_spacing = large_button_height * 0.2
         
-        play_button_x = int(sidebar_width * 0.5 - play_button_width * 0.5)
-        play_button_spacing = play_button_height * 0.2
-        first_play_button_y = int(self.height * 0.35)
+        large_button_x = int(sidebar_width * 0.5 - large_button_width * 0.5)
+        first_large_button_y = int(self.height * 0.35)
 
-        # Obtém o tamanho e posição do botão de configuração.
-        settings_button_width = int(sidebar_width * 0.18)
-        settings_button_height = int(settings_button_width * 0.75)
+        # Obtém o tamanho e posição dos botões menores, que serão dispostos horizontalmente.
+        small_button_width = (large_button_width * 0.8) // 3
+        small_button_height = int(small_button_width * 0.75)
+        small_button_spacing = large_button_width * 0.1
 
-        settings_button_x = int(sidebar_width - (sidebar_width - play_button_width) / 2 - settings_button_width)
-        settings_button_y = int(sidebar_height * 0.9 - settings_button_height)
+        first_small_button_x = large_button_x
+        small_button_y = int(sidebar_height * 0.9 - small_button_height)
 
         # Obtém o tamanho e a posição da caixa de mensagem.
         message_box_width = self.width * 0.45
@@ -65,42 +66,63 @@ class HomeScreen(Screen):
         logo_sprite = self.create_sprite(logo_image, batch = batch, x = logo_x, y = logo_y)
 
         # Carrega a imagem dos botões de jogar.
-        button_1_filename = application.paths.get_image("home", "buttons", "play_local.png")
-        activated_button_1_filename = application.paths.get_image("home", "buttons", "activated_play_local.png")
+        play_button_1_filename = application.paths.get_image("home", "buttons", "play_local.png")
+        activated_play_button_1_filename = application.paths.get_image("home", "buttons", "activated_play_local.png")
         
-        button_2_filename = application.paths.get_image("home", "buttons", "play_as_host.png")
-        activated_button_2_filename = application.paths.get_image("home", "buttons", "activated_play_as_host.png")
+        play_button_2_filename = application.paths.get_image("home", "buttons", "play_as_host.png")
+        activated_play_button_2_filename = application.paths.get_image("home", "buttons", "activated_play_as_host.png")
         
-        button_3_filename = application.paths.get_image("home", "buttons", "play_as_client.png")
-        activated_button_3_filename = application.paths.get_image("home", "buttons", "activated_play_as_client.png")
+        play_button_3_filename = application.paths.get_image("home", "buttons", "play_as_client.png")
+        activated_play_button_3_filename = application.paths.get_image("home", "buttons", "activated_play_as_client.png")
+
+        # Carrega a imagem dos botões de histórico, conquistas e configurações.
+        history_button_filename = application.paths.get_image("home", "buttons", "history.png")
+        activated_history_button_filename = application.paths.get_image("home", "buttons", "activated_history.png")
+
+        achivements_button_filename = application.paths.get_image("home", "buttons", "achivements.png")
+        activated_achivements_button_filename = application.paths.get_image("home", "buttons", "activated_achivements.png")
 
         settings_button_filename = application.paths.get_image("home", "buttons", "settings.png")
         activated_settings_button_filename = application.paths.get_image("home", "buttons", "activated_settings.png")
 
-        button_1 = Button(
-            self, batch, play_button_x,
-            first_play_button_y,
-            (play_button_width, play_button_height),
-            (button_1_filename, activated_button_1_filename)
+        # Cria os botões de jogar.
+        play_button_1 = Button(
+            self, batch, large_button_x,
+            first_large_button_y + (large_button_height + large_button_spacing) * 0,
+            (large_button_width, large_button_height),
+            (play_button_1_filename, activated_play_button_1_filename)
         )
 
-        button_2 = Button(
-            self, batch, play_button_x,
-            first_play_button_y + (play_button_height + play_button_spacing) * 1,
-            (play_button_width, play_button_height),
-            (button_2_filename, activated_button_2_filename)
+        play_button_2 = Button(
+            self, batch, large_button_x,
+            first_large_button_y + (large_button_height + large_button_spacing) * 1,
+            (large_button_width, large_button_height),
+            (play_button_2_filename, activated_play_button_2_filename)
         )
 
-        button_3 = Button(
-            self, batch, play_button_x,
-            first_play_button_y + (play_button_height + play_button_spacing) * 2,
-            (play_button_width, play_button_height),
-            (button_3_filename, activated_button_3_filename)
+        play_button_3 = Button(
+            self, batch, large_button_x,
+            first_large_button_y + (large_button_height + large_button_spacing) * 2,
+            (large_button_width, large_button_height),
+            (play_button_3_filename, activated_play_button_3_filename)
+        )
+
+        # Cria os botões de histórico, conquistas e configurações.
+        history_button = Button(
+            self, batch, first_small_button_x + (small_button_width + small_button_spacing) * 0,
+            small_button_y, (small_button_width, small_button_height),
+            (history_button_filename, activated_history_button_filename)
+        )
+
+        achivements_button = Button(
+            self, batch, first_small_button_x + (small_button_width + small_button_spacing) * 1,
+            small_button_y, (small_button_width, small_button_height),
+            (achivements_button_filename, activated_achivements_button_filename)
         )
 
         settings_button = Button(
-            self, batch, settings_button_x, settings_button_y,
-            (settings_button_width, settings_button_height),
+            self, batch, first_small_button_x + (small_button_width + small_button_spacing) * 2,
+            small_button_y, (small_button_width, small_button_height),
             (settings_button_filename, activated_settings_button_filename)
         )
 
@@ -129,19 +151,29 @@ class HomeScreen(Screen):
         self.__logo_sprite = logo_sprite
         self.__message_box = message_box
         
-        self.__button_1 = button_1
-        self.__button_2 = button_2
-        self.__button_3 = button_3
+        self.__play_button_1 = play_button_1
+        self.__play_button_2 = play_button_2
+        self.__play_button_3 = play_button_3
+
+        self.__history_button = history_button
+        self.__achivements_button = achivements_button
         self.__settings_button = settings_button
         
         self.__batch = batch
 
     def __check_buttons(self, x, y):
-        if self.__button_1.check(x, y): return True, False, False, False
-        elif self.__button_2.check(x, y): return False, True, False, False
-        elif self.__button_3.check(x, y): return False, False, True, False
-        elif self.__settings_button.check(x, y): return False, False, False, True
-        return False, False, False, False
+        # A princípio pode parecer exagerado definir múltiplas condicionais,
+        # ao invés de simplesmente retornar em uma única linha o retorno de cada função,
+        # no entanto, cada verificação abaixo, repetidas várias vezes, acaba sendo muito
+        # custoso. Só é necessário verificar uma vez. Dessa forma, o programa fica mais
+        # otimizado e a animação da tela, consequentemente, fica mais fluída.
+        if self.__play_button_1.check(x, y): return True, False, False, False, False, False
+        elif self.__play_button_2.check(x, y): return False, True, False, False, False, False
+        elif self.__play_button_3.check(x, y): return False, False, True, False, False, False
+        elif self.__history_button.check(x, y): return False, False, False, True, False, False
+        elif self.__achivements_button.check(x, y): return False, False, False, False, True, False
+        elif self.__settings_button.check(x, y): return False, False, False, False, False, True
+        return False, False, False, False, False, False
 
     def set_message(self, *message):
         self.__message_box.set_message(
@@ -158,14 +190,15 @@ class HomeScreen(Screen):
         x, y, mouse_button = super().on_mouse_release(*args)[0: 3]
         if mouse_button != mouse.LEFT: return
             
-        button_1, button_2, button_3, settings_button = self.__check_buttons(x, y)
+        play_button_1, play_button_2, play_button_3, history, achivements, settings = self.__check_buttons(x, y)
 
+        # Qualquer ação será realizada somente se não houver mensagens sendo mostrada na tela.
         if self.__message_box.has_message():
             return self.__message_box.delete_message()
         
-        if button_1: self.__on_play(1)
-        elif button_2: self.__on_play(2)
-        elif button_3: self.__on_play(3)
+        if play_button_1: self.__on_play(1)
+        elif play_button_2: self.__on_play(2)
+        elif play_button_3: self.__on_play(3)
          
     def on_draw(self, by_scheduler = False):
         if by_scheduler: self.__background.next()
