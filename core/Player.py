46594f408c99b7492c6263bc3b5ee740adff555e
Izak76 <izakalvesgama@outlook.com>
2022-10-12 11:11:31 -0300

    Classe Player (jogador) atualizada com novos recursos

--- core/Player.py
+++ core/Player.py
@@ -1,4 +1,21 @@
 class Player:
-    def __init__(self, color):
+    def __init__(self, color): #color = "black" ou color = "white"
+        if not color in ("black", "white"):
+            raise ValueError("\"color\" must be \"black\" or \"white\".")
+
         self.color = color
-        self.pieces = []
+        self.__played = False
+
+    def __bool__(self):
+        return self.__played
+        
+    @property
+    def played(self):
+        return self.__played
+
+    @played.setter
+    def played(self, value):
+        if not isinstance(value, bool):
+            raise ValueError("The \"played\" attribute must be a bool.")
+
+        self.__played = value
