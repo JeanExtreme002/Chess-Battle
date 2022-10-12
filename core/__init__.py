8dcd279ad3ef13116fff6ea46b002266823f3893
Izak76 <izakalvesgama@outlook.com>
2022-10-12 11:12:40 -0300

    Classe principal atualizada

--- core/__init__.py
+++ core/__init__.py
@@ -4,25 +4,38 @@ from Piece import Piece
 from enum import Enum
 
 class ChessGame:
-    def __init__(self, white_player: Player, black_player: Player, database_path: str):
+    def __init__(self, database_path: str):
         self.database_path = database_path
-        self.white_player = white_player
-        self.black_player = black_player
+        self.__white_player = Player("white")
+        self.__black_player = Player("black")
 
-    def new_game(self, first_player:Enum, timeout=None):
-        pass
+    @property
+    def white_player(self):
+        return self.__white_player
 
-    def load_game(self, match:int, round:int):
-        pass
+    @property
+    def black_player(self):
+        return self.__black_player
+    
+    def new_game(self, first_player=None, timeout=None):
+        if first_player == None:
+            first_player = self.__white_player
+        self.__white_player.played = True #Assumimos que o método get_player será chamado após o 1º jogador fazer um movimento
 
-    def get_history(self) -> list:
-        pass
+    def load_game(self, match:int, round:int): #Esperando database ficar pronto (ou quase isso)...
+        return NotImplemented
 
-    def get_player(self) -> Enum:
-        pass
+    def get_history(self) -> list: #Mesma coisa do de cima...
+        return NotImplemented
 
-    def get_time(self) -> str:
-        pass
+    def get_player(self) -> Player:
+        self.__white_player.played = not self.__white_player.played
+        self.__black_player.played = not self.__black_player.played
+
+        return self.__white_player or self.__black_player
+
+    def get_time(self) -> str: #Esperando implementarem o tempo...
+        return NotImplemented
 
     def get_piece(self, x:int, y:int) -> Union[Enum, Piece]:
         pass
