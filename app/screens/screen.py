74b7d061fe76bf2941a54573a471f4c170afd7e8
JeanExtreme002 <jeangamerextreme@gmail.com>
2022-10-13 11:32:32 -0300

    optimized image loader

--- app/screens/screen.py
+++ app/screens/screen.py
@@ -14,9 +14,11 @@ class Screen(ABC):
     """
     Classe para gerar telas.
     """
+
+    _images = dict()
+    
     def __init__(self, application):
         self.__application = application
-        self.__images = dict()
         
     @property
     def width(self):
@@ -56,11 +58,11 @@ class Screen(ABC):
     def load_image(self, filename, size = None, save = True):
 
         # Carrega a imagem.
-        if not filename in self.__images:
+        if not filename in Screen._images:
             img = image.load(filename)
-            if save: self.__images[filename] = img
+            if save: Screen._images[filename] = img
         else:
-            img = self.__images[filename]
+            img = Screen._images[filename]
 
         # Redefine o seu tamanho, caso solicitado, alterando a sua escala.
         if size:
