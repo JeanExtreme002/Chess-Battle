1574e2186985e273cff37c83a5da840ceed9fc3d
Izak76 <izakalvesgama@outlook.com>
2022-10-15 11:08:21 -0300

    Fusão dos arquivos dos branches main e data_base + melhorada

--- core/teste.py
+++ core/teste.py
@@ -1,15 +1,48 @@
-from Piece import Piece
-from Pawn import Pawn
-from Rook import Rook
+from .Piece import Piece
+from .Pawn import Pawn
+from .Rook import Rook
 from enum import Enum
-from Queen import Queen
-from Color import Color
-
+from .Queen import Queen
+from .Knight import Knight
+from .Bishop import Bishop
+from .King import King
+from .Color import Color
 
 class Board():
-
     def __init__(self):
-        self.pecas = [[None] * 8 for i in range(8)]
+        self.pecas = [[0]*8 for i in range(8)]
+
+        for i in range(8):       
+            wp = Pawn(Color.White,i,1)
+            self.pecas[1][i] = wp
+
+        for i in range(8):       
+            bp = Pawn(Color.Black,i,1)
+            self.pecas[6][i] = bp
+
+        wr0 = Rook(Color.White,0,0)
+        wr1 = Rook(Color.White,0,7)
+        br0 = Rook(Color.Black,7,0)
+        br1 = Rook(Color.Black,7,7)
+
+        wn0 = Knight(Color.White,0,1)
+        wn1 = Knight(Color.White,0,6)
+        bn0 = Knight(Color.Black,7,1)
+        bn1 = Knight(Color.Black,7,6)
+
+        wb0 = Bishop(Color.White,0,2)
+        wb1 = Bishop(Color.White,0,5)
+        bb0 = Bishop(Color.Black,7,2)
+        bb1 = Bishop(Color.Black,7,5)
+
+        wk = King(Color.White,0,3)
+        bk = King(Color.Black,7,4)
+
+        wq = Queen(Color.White,0,4)
+        bq = Queen(Color.Black,7,3)
+
+        for p in (wr0, wr1, br0, br1, wn0, wn1, bn0, bn1, wb0, wb1, bb0, bb1, bq, wq, bk, wk):
+            self.add_piece(p)
 
     def add_piece(self, piece):
         self.pecas[piece.x][piece.y] = piece.r_id
@@ -18,12 +51,16 @@ class Board():
         # for i in self.pecas:
         #     print(i.r_id)
         for i in range(8):
-            print(self.pecas[i])
+            for j in range(8):
+                if type(self.pecas[i][j]) == int:
+                    print(f"{self.pecas[i][j]:02d}", end=" ")
+                else:
+                    print(self.pecas[i][j].r_id, end=" ")
+            print()
 
     def write_pos(self):
         for i in range(8):
             print(self.pecas[i])
-
 
 class Data():
     game_number = 0
@@ -33,12 +70,6 @@ class Data():
         file = ""
         return
 
-
-tabuleiro = Board()
-joao = Pawn(Color.White, 0, 0)
-jean = Pawn(Color.Black, 0, 1)
-nanda = Queen(Color.Black, 2, 0)
-tabuleiro.add_piece(joao)
-tabuleiro.add_piece(jean)
-tabuleiro.add_piece(nanda)
-tabuleiro.imprimir()
+if __name__ == '__main__':
+    tabuleiro = Board()
+    tabuleiro.imprimir()
