299a0196ff7836fee10d5cd21475fb390374abeb
JeanExtreme002 <jeangamerextreme@gmail.com>
2022-11-28 01:07:20 -0300

    added info file

--- Xadrez.py
+++ Xadrez.py
@@ -5,6 +5,17 @@ from locale import getlocale
 import sys
 import traceback
 
+__project__ = "https://github.com/JeanExtreme002/Chess-Battle"
+__version__ = "2.0.0"
+
+def generate_info_file(filename: str):
+    """
+    Função para gerar arquivo com as informações do jogo.
+    """
+    with open(filename, "w") as file:
+        file.write("Project URL: {}\n".format(__project__))
+        file.write("VERSION: {}\n".format(__version__))
+
 def main():
     """
     Função principal para executar o jogo.
@@ -17,7 +28,7 @@ def main():
     application = Application(title, chess_game, winter_theme = winter_theme)
     application.run()
 
-def save_errors_to_file(filename):
+def save_errors_to_file(filename: str):
     """
     Salva o erro gerado em um arquivo.
     """
@@ -28,7 +39,9 @@ def save_errors_to_file(filename):
 # Executa o código principal e, caso haja erro,
 # o mesmo será salvo em um arquivo.
 try:
+    generate_info_file("info.txt")
     main()
 except Exception:
     print("It looks like the game has crashed due to an error. Check it in the log file.")
     save_errors_to_file(filename = "log.txt")
+
